a7a7b21b197f45d10afa3ec14f54ee33eb2529fb
KienHungPG <voxkienhung@gmail.com>
2023-11-27 17:06:29 +0700

    update

--- ss8_validate/exercise/register/src/main/java/com/codegym/register/dto/UserDto.java
+++ ss8_validate/exercise/register/src/main/java/com/codegym/register/dto/UserDto.java
@@ -1,9 +1,27 @@
 package com.codegym.register.dto;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-public class UserDto {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto implements Validator {
     @NotEmpty
     @Size(min = 5, max = 45, message = "required, minimum length 5, maximum 45 characters")
     private String firstName;
@@ -12,59 +30,34 @@ public class UserDto {
     private String lastName;
     @Pattern(regexp = "^0[0-9]{9}$", message = "The phone number must be 10 digits long, starting with 0")
     private String phoneNumber;
-    @Min(value = 18, message = "Must be over the age of 18")
-    private int age;
+    @Past(message = "Birthdate must be in the past")
+    private Date birthDate;
     @Email(message = "Email format is incorrect")
     private String email;
 
-    public UserDto(String firstName, String lastName, String phoneNumber, int age, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.age = age;
-        this.email = email;
+    public boolean isOver18() {
+        if (birthDate == null) {
+            return false;
+        }
+
+        LocalDate dobLocalDate = birthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate now = LocalDate.now();
+
+        Period period = Period.between(dobLocalDate, now);
+        return period.getYears() >= 18;
+    }
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
     }
 
-    public UserDto() {
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public void validate(Object target, Errors errors) {
+        UserDto userDto = (UserDto) target;
+        if (userDto.getBirthDate() == null){
+            errors.rejectValue("birthDate", null, "Please enter birthday");
+        } else if (!userDto.isOver18()) {
+            errors.rejectValue("birthDate",null,"Your age must be greater than 18 years old");
+        }
     }
 }
